c9866c578845e3471f40ebad04adc2a9f7bbb2cc
Rico Mercado <74946193+mercadorico@users.noreply.github.com>
2021-11-22 01:13:06 +0800

    Style navbar component according to design.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './navbar.css';
+import logo from '../../assets/shared/logo.svg'
 
 function Navbar() {
     return (
-        <div>
-            Navbar
+        <div className="navbar navbar--flex-container">
+            <img src={logo} alt='logo' />
+            <hr className='navbar__hr'/>
+            <div className='nav navbar__nav--blur'>
+                <div className='nav__links nav__links--flex'>
+                    <a href='/'> <strong>00</strong> HOME</a>
+                    <a href='/destination'> <strong>01</strong> DESTINATION</a>
+                    <a href='/crew'> <strong>02</strong> CREW</a>
+                    <a href='/technology'> <strong>03</strong> TECHNOLOGY</a>
+                </div>
+            </div>
         </div>
     )
 }
