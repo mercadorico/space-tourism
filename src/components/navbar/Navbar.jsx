5bf81854b3e351b63fe37ef42193ac64b80af150
Rico Mercado <74946193+mercadorico@users.noreply.github.com>
2021-11-28 02:43:08 +0800

    Make nav and hr responsive to ipad and when shrinking.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,13 +6,15 @@ function Navbar() {
     return (
         <div className="navbar navbar--flex-container">
             <img src={logo} alt='logo' />
-            <hr className='navbar__hr'/>
-            <div className='nav navbar__nav--blur'>
-                <div className='nav__links nav__links--flex'>
-                    <a href='/'> <strong>00</strong> HOME</a>
-                    <a href='/destination'> <strong>01</strong> DESTINATION</a>
-                    <a href='/crew'> <strong>02</strong> CREW</a>
-                    <a href='/technology'> <strong>03</strong> TECHNOLOGY</a>
+            <div className="navbar2--hide-items">
+                <hr className='navbar2__hr--flex-item'/>
+                <div className='nav navbar__nav--blur navbar2__nav--flex-item'>
+                    <div className='nav__links nav__links--flex'>
+                        <a href='/'> <strong>00</strong> HOME</a>
+                        <a href='/destination'> <strong>01</strong> DESTINATION</a>
+                        <a href='/crew'> <strong>02</strong> CREW</a>
+                        <a href='/technology'> <strong>03</strong> TECHNOLOGY</a>
+                    </div>
                 </div>
             </div>
         </div>
