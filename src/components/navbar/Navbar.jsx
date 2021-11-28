84a284f0440323cc7fbe9702a38649e663eabddd
Rico Mercado <74946193+mercadorico@users.noreply.github.com>
2021-11-28 23:11:58 +0800

    Add toggle-menu using state in mobile mode.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css';
 import logo from '../../assets/shared/logo.svg'
 import menu from '../../assets/shared/icon-hamburger.svg';
+import close from '../../assets/shared/icon-close.svg';
+
+function Menu() {
+    return (
+        <>
+            <a href='/'> <strong>00</strong> HOME</a>
+            <a href='/destination'> <strong>01</strong> DESTINATION</a>
+            <a href='/crew'> <strong>02</strong> CREW</a>
+            <a href='/technology'> <strong>03</strong> TECHNOLOGY</a>
+        </>
+    );
+}
 
 function Navbar() {
+    const [toggleMenu, setToggleMenu] = useState(false); 
+
     return (
         <div className="navbar navbar--flex-container">
+            {/* image container */}
             <div className='navbar__img-container--flex-item'>
                 <img src={logo} alt='logo' />
             </div>
 
-            <div className='navbar__menu navbar__menu--display'>
-                <img src={menu} alt='navbar__menu'/>
+            {/* navbar menu for mobile */}
+            <div className={`navbar__menu navbar__menu--display ${toggleMenu && 'navbar__menu--blur'} navbar__menu--flex-container`}>
+                <div className='navbar__menu--flex-item'>
+                    {
+                        toggleMenu ? 
+                            <img src={close} onClick = {() => {setToggleMenu(false)}} className='menu-logo' alt='navbar__menu'/> 
+                            : <img src={menu} onClick = {() => {setToggleMenu(true)}} className='menu-logo' alt='navbar__menu'/>
+                    }
+                </div>
+                {
+                    toggleMenu && (
+                        <div className='menu__links--flex-container'>
+                            <Menu />
+                        </div>              
+                    )
+                }
             </div>
             
+            {/* navbar menu for desktop, laptop, and tablet */}
             <div className="navbar2--hide-items">
                 <hr className='navbar2__hr--flex-item'/>
                 <div className='nav navbar__nav--blur navbar2__nav--flex-item'>
                     <div className='nav__links nav__links--flex'>
-                        <a href='/'> <strong>00</strong> HOME</a>
-                        <a href='/destination'> <strong>01</strong> DESTINATION</a>
-                        <a href='/crew'> <strong>02</strong> CREW</a>
-                        <a href='/technology'> <strong>03</strong> TECHNOLOGY</a>
+                        <Menu />
                     </div>
                 </div>
             </div>
