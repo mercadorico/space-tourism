12860356e8f1513d58bf7aac4f6ad9e66ad023be
Rico Mercado <74946193+mercadorico@users.noreply.github.com>
2021-12-10 17:38:01 +0800

    Add buttons with event trigger that sets index-state to select destination details from the array.

--- src/components/destination/Destination.jsx
+++ src/components/destination/Destination.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import './destination.css';
 
-import {Europa, Mars, Moon, Titan} from "../../assets/exportDestination";
+import {moonDetails, marsDetails, europaDetails, titanDetails} from './exportDetails';
 
-function Destination() {
+function Destination(props) {
     // Remove class and add new one to render correct background image
     document.body.classList.remove("home", "crew", "technology");
     document.body.classList.add("destination");
 
+    // array to hold destination imgs
+    const destinationDetails = [moonDetails, marsDetails, europaDetails, titanDetails];
+
     return (
         <div>
-            <img src={Moon} alt='moon-img' />
+            <img src={destinationDetails[props.selectIndex].img} alt='destination-img' />
+            <h1>{destinationDetails[props.selectIndex].name}</h1>
+            <div className='button__container'>
+                {/* When you need to invoke a function that needs an argument, wrap it inside another function.*/}
+                <button onClick={() => {props.handleClick(0)}}>MOON</button>
+                <button onClick={() => {props.handleClick(1)}}>MARS</button>
+                <button onClick={() => {props.handleClick(2)}}>EUROPA</button>
+                <button onClick={() => {props.handleClick(3)}}>TITAN</button>
+            </div>
         </div>
     )
 }
---
+++ src/components/destination/exportDetails.js
@@ -0,0 +1,35 @@
+import {Europa, Mars, Moon, Titan} from "../../assets/exportDestination";
+
+const moonDetails = {
+    img: Moon,
+    name: 'MOON',
+    description: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+    distance: '384, 400',
+    travelTime: '3 DAYS'
+}
+
+const marsDetails = {
+    img: Mars,
+    name: 'MARS',
+    description: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+    distance: '225 MIL.',
+    travelTime: '9 MONTHS'
+}
+
+const europaDetails = {
+    img: Europa,
+    name: 'EUROPA',
+    description: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+    distance: '628 MIL.',
+    travelTime: '3 YEARS'
+}
+
+const titanDetails = {
+    img: Titan,
+    name: 'TITAN',
+    description: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+    distance: '1.6 BIL.',
+    travelTime: '7 YEARS'
+}
+
+export {moonDetails, marsDetails, europaDetails, titanDetails};
