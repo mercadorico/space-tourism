7d1a8f435f51dc214b8d22079d17aebeac2b6e5a
Rico Mercado <74946193+mercadorico@users.noreply.github.com>
2021-12-13 00:36:01 +0800

    Align media queries of components.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,14 +8,16 @@ function Home() {
     document.body.classList.add("home");
 
     return (
-        <div className="home--flex-container">
-            <div className='home__text'>
-                <div className='home__firstSentence'>SO, YOU WANT TO TRAVEL TO</div>
-                <div className='home__space'>SPACE</div>
-                <div className='home__paragraph'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</div>
-            </div>
-            <div className='home__explore--flex-container home__explore--flex-item'>
-                <div className='home__explore--flex-container2'>EXPLORE</div>
+        <div>
+            <div className="home--flex-container">
+                <div className='home__text'>
+                    <div className='home__firstSentence'>SO, YOU WANT TO TRAVEL TO</div>
+                    <div className='home__space'>SPACE</div>
+                    <div className='home__paragraph'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</div>
+                </div>
+                <div className='home__explore--flex-container home__explore--flex-item'>
+                    <div className='home__explore--flex-container2'>EXPLORE</div>
+                </div>
             </div>
         </div>
     )
