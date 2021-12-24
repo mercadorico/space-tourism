908a704ac73379de6afcad13e6eddd77f4141563
Rico Mercado <74946193+mercadorico@users.noreply.github.com>
2021-12-25 03:22:13 +0800

    Structure and import assets to technology page.

---
+++ src/assets/exportTechnology.js
@@ -0,0 +1,3 @@
+export {default as Launch_Vehicle} from './technology/image-launch-vehicle-landscape.jpg';
+export {default as Space_Capsule} from './technology/image-space-capsule-landscape.jpg';
+export {default as Spaceport} from './technology/image-spaceport-landscape.jpg';
--- src/components/technology/Technology.jsx
+++ src/components/technology/Technology.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import './technology.css';
 
-function Technology() {
+import {vehicleDetails, capsuleDetails, portDetails} from './exportDetails';
+
+function Technology(props) {
     // Remove class and add new one to render correct background image
     document.body.classList.remove("home", "crew", "destination");
     document.body.classList.add("technology");
 
+    const technologyDetails = [vehicleDetails, portDetails, capsuleDetails];
+
     return (
         <div>
-            Technology
+            <div className='technology-header'><strong>02</strong> SPACE LAUNCH 101</div>
+
+            <div className='technology__flex-container'>
+                <div className='technology__img-container'>
+                    <img className='landscape' src={technologyDetails[0].img_landscape} alt='crew-img'/>
+                </div>
+
+                <div className='details__flex-container'>
+                    <div className='button__flex-container'>
+                        <button className='technology__button'>1</button>
+                        <button className='technology__button'>2</button>
+                        <button className='technology__button'>3</button>
+                    </div>
+
+                    <div>THE TERMINOLOGY...</div>
+
+                    <div>{technologyDetails[0].name}</div>
+
+                    <p>{technologyDetails[0].description}</p>
+                </div>
+            </div>
         </div>
     )
 }
