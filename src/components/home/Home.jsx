3a17ed4f0aac03840ee1acbc4f0dce2239593ec8
Rico Mercado <74946193+mercadorico@users.noreply.github.com>
2021-12-31 22:18:14 +0800

    Add interactivity to explore button.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -15,9 +15,12 @@ function Home() {
                     <div className='home__space'>SPACE</div>
                     <div className='home__paragraph'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</div>
                 </div>
-                <div className='home__explore--flex-container home__explore--flex-item'>
-                    <div className='home__explore--flex-container2'>EXPLORE</div>
-                </div>
+
+                    <div className='home__explore--flex-container home__explore--flex-item'>
+                        <a href='/destination'>
+                            <div className='home__explore--flex-container2'>EXPLORE</div>
+                        </a>
+                    </div>
             </div>
         </div>
     )
